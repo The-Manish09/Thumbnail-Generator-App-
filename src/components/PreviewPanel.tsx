c8fc6121f791f491141b1adcaacd79b3f3e2e992
The-Manish09 <manishchauhan22.9mc@gmail.com>
2026-01-16 23:56:17 +0530

    Preview panel

---
+++ src/components/PreviewPanel.tsx
@@ -0,0 +1,11 @@
+import type { IThumbnail } from "../assets/assets"
+
+const PreviewPanel = ({thumbnail, isLoading, aspectRatio} : {thumbnail: IThumbnail, isLoading}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PreviewPanel
